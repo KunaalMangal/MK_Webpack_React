320b8b6530465fb126bbed159050cd4f16a166f5
KUNAAL MANGAL <ksmangal1998@gmail.com>
2025-09-18 07:02:14 +0530

    - publish production build to gh-pages

--- src/pages/index.ts
+++
@@ -1,6 +0,0 @@
-// Pages Components
-export { default as Home } from './home/Home';
-export { default as About } from './about/About';
-export { default as Author } from './author/Author';
-export { default as Contact } from './contact/Contact';
-export { default as NotFound } from './not-found/NotFound';
